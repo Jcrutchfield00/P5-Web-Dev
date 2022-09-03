4b4d0369aed604e789b651a47259f886a55350d5
Jennifer Crutchfield <Jcrutchfield00@gmail.com>
2022-09-03 10:56:16 -0500

    added dropdown menu for colors aswell as event listener to addtocart button

--- back/product.js
+++ back/product.js
@@ -28,8 +28,20 @@ var data = " ";
     var productImage = document.createElement('img');
     var parentDiv = document.querySelector('div.item__img');
     parentDiv.appendChild(productImage);
+    //below create the color option for dropdown menu
+    var select = document.getElementById("colors"); 
+    var colorOption = data.colors; 
+    console.log(colorOption);
+    var i;
 
-    
+    for(i = 0; i < colorOption.length; i++) {
+        var colors = colorOption[i];
+        var optionElement = document.createElement("option");
+        optionElement.textContent = colors;
+        optionElement.value = colors;
+        select.appendChild(optionElement);
+    }
+    //above code ends dropdown menu
      productImage.setAttribute('src', data.imageUrl);
     productImage.setAttribute('alt', data.altTxt);
 productTitle.innerHTML = data.name;
@@ -37,3 +49,10 @@ productTitle.innerHTML = data.name;
     productPrice.innerHTML = data.price;
 };
 
+    
+
+//addtocartbutton isworking but need to figure out howto get it to the cart page
+document.getElementById('addToCart').addEventListener("click", clickAddToCart)
+function clickAddToCart(){
+ console.log("Hi there")
+}
