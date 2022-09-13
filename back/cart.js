cb3695778c19ccbfc659a530ec858fd0c210e851
Jennifer Crutchfield <Jcrutchfield00@gmail.com>
2022-09-13 18:17:51 -0500

    somewhat working cart and product.js

--- back/cart.js
+++ back/cart.js
@@ -1,72 +1,96 @@
 // api url
 const api_url = "http://localhost:3000/api/products";
 
+
+
 // Defining async function
 async function getapi(url) {
   // Storing response
   const response = await fetch(url);
-
+console.log(url);
   // Storing data in form of JSON
   var data = await response.json();
   console.log(data);
   if (response) {
+    show(data);
+    getCartStorage();
   }
-  show(data);
 }
-// Calling that async function
+// // Calling that async function
 getapi(api_url);
 
+//example
 
+//below sets the html elements dynamically
 function show(data) {
-    //below is the code for first block of section id=cart__items
-      var newArticle = document.createElement("article");
-      var cartItemImgDiv = document.createElement("div");
-      var newImage = document.createElement("img");
-      var cartItems = document.getElementById("cart__items");
-      newArticle.classList.add("cart__item");
-      cartItemImgDiv.classList.add("cart__item__img");
-      cartItems.appendChild(newArticle);
-      newArticle.appendChild(cartItemImgDiv);
-      cartItemImgDiv.appendChild(newImage);
-      newImage.setAttribute("src", data.imageUrl);
-      newImage.setAttribute("alt", data.altTxt);
-   
-    //below is the code for second block of section id=cart__items
-    var cartItemContentDiv = document.createElement("div");
-    var cartItemContentDescription = document.createElement("div");
-    var newHeading = document.createElement("h2");
-    var newParagraphColor = document.createElement("p");
-    var newParagraphPrice = document.createElement("p");
-    cartItemContentDiv.classList.add("cart__item__content");
-    cartItemContentDescription.classList.add("cart__item__content__description");
-    
-      newArticle.appendChild(cartItemContentDiv);
-      cartItemContentDiv.appendChild(cartItemContentDescription);
-      cartItemContentDescription.appendChild(newHeading);
-      cartItemContentDescription.appendChild(newParagraphColor);
-      cartItemContentDescription.appendChild(newParagraphPrice);
-
-      //below is the code for the third block of section id=cart__items
-      var cartItemContentSettings = document.createElement("div");
-      var cartItemContentSettingsQuantity = document.createElement("div");
-      var newParagraphQty = document.createElement("p");
-      var input = document.createElement("input");
-      cartItemContentSettings.classList.add("cart__item__content__settings");
-      cartItemContentSettingsQuantity.classList.add("cart__item__content__settings__quantity");
-      
-        cartItemContentDiv.appendChild(cartItemContentSettings);
-        cartItemContentSettings.appendChild(cartItemContentSettingsQuantity);
-        cartItemContentSettingsQuantity.appendChild(newParagraphQty);
-        cartItemContentSettingsQuantity.appendChild(input);
-        
-   //below is the code for the fourth block of section id=cart__items
-     var cartItemContentSettingsDelete = document.createElement("div");
-      var newParagraphDelete = document.createElement("p");
-    cartItemContentSettingsDelete.classList.add("cart__item__content__settings__delete");
-     newParagraphDelete.classList.add("deleteItem");
-     cartItemContentSettings.appendChild(cartItemContentSettingsDelete);
-     cartItemContentSettingsDelete.appendChild(newParagraphDelete);
-
-
-    };
+   // const getCartItems = (data) => {
+   //below is the code for first block of section id=cart__items
+     var newArticle = document.createElement("article");
+     var cartItemImgDiv = document.createElement("div");
+     var newImage = document.createElement("img");
+     var cartItems = document.getElementById("cart__items");
+     newArticle.classList.add("cart__item");
+     cartItemImgDiv.classList.add("cart__item__img");
+     cartItems.appendChild(newArticle);
+     newArticle.appendChild(cartItemImgDiv);
+     cartItemImgDiv.appendChild(newImage);
+     newImage.setAttribute("src", data.imageUrl);
+     newImage.setAttribute("alt", data.altTxt);
   
+   //below is the code for second block of section id=cart__items
+   var cartItemContentDiv = document.createElement("div");
+   var cartItemContentDescription = document.createElement("div");
+   var newHeading = document.createElement("h2");
+   var newParagraphColor = document.createElement("p");
+   var newParagraphPrice = document.createElement("p");
+   cartItemContentDiv.classList.add("cart__item__content");
+   cartItemContentDescription.classList.add("cart__item__content__description");
+   
+     newArticle.appendChild(cartItemContentDiv);
+     cartItemContentDiv.appendChild(cartItemContentDescription);
+     cartItemContentDescription.appendChild(newHeading);
+     cartItemContentDescription.appendChild(newParagraphColor);
+     cartItemContentDescription.appendChild(newParagraphPrice);
+
+     //below is the code for the third block of section id=cart__items
+     var cartItemContentSettings = document.createElement("div");
+     var cartItemContentSettingsQuantity = document.createElement("div");
+     var newParagraphQty = document.createElement("p");
+     var input = document.createElement("input");
+     cartItemContentSettings.classList.add("cart__item__content__settings");
+     cartItemContentSettingsQuantity.classList.add("cart__item__content__settings__quantity");
+     
+       cartItemContentDiv.appendChild(cartItemContentSettings);
+       cartItemContentSettings.appendChild(cartItemContentSettingsQuantity);
+       cartItemContentSettingsQuantity.appendChild(newParagraphQty);
+       cartItemContentSettingsQuantity.appendChild(input);
+       
+  //below is the code for the fourth block of section id=cart__items
+    var cartItemContentSettingsDelete = document.createElement("div");
+     var newParagraphDelete = document.createElement("p");
+   cartItemContentSettingsDelete.classList.add("cart__item__content__settings__delete");
+    newParagraphDelete.classList.add("deleteItem");
+    cartItemContentSettings.appendChild(cartItemContentSettingsDelete);
+    cartItemContentSettingsDelete.appendChild(newParagraphDelete);
+
+   newHeading.innerHTML = data.name;
+   newParagraphColor.innerHTML = data.colors;
+   newParagraphPrice.innerHTML = data.price;
+    }
+    
+  // newParagraphQty.innerHTML = qty;
+  
+  //  data.forEach((data) => getCartItems(data));
+  // };
+   //cartItems.item.push({"id" : id, "color" : selectColor, "quantity" : selectQuantity})
+
+   function getCartStorage(){
+    if(localStorage) {
+ 
+       const cartItemContent = JSON.parse(window.localStorage.getItem('cart'));
+        console.log("Hi, " + cartItemContent);
+        
+    } else {
+        alert("Sorry, your browser do not support local storage.");
+    }
+  };
--- back/product.js
+++ back/product.js
@@ -3,12 +3,12 @@ var str = window.location.href;
 console.log(str);
 var url = new URL(str);
 console.log(url);
-var idProduct = url.searchParams.get("id");
-console.log(idProduct);
+var productId = url.searchParams.get("id");
+console.log(productId);
 var data = " ";
 
   
-    fetch("http://localhost:3000/api/products/" + idProduct)
+    fetch("http://localhost:3000/api/products/" + productId)
     .then((data) => {
         return data.json();
     })
@@ -54,5 +54,38 @@ productTitle.innerHTML = data.name;
 //addtocartbutton isworking but need to figure out howto get it to the cart page
 document.getElementById('addToCart').addEventListener("click", clickAddToCart)
 function clickAddToCart(){
- console.log("Hi there")
-}
+    let cart = [];
+   
+   
+    const selectColor = colors.value;
+    console.log(selectColor);
+    const selectQuantity = document.getElementById("quantity").value
+    console.log(selectQuantity);
+    if(localStorage.getItem('products')) {
+
+    
+           cartItemContent = JSON.parse(localStorage.getItem);
+           console.log("Hi, " + cartItemContent);
+    }     
+    cart.push({"id" : productId, "color" : selectColor, "quantity" : selectQuantity})
+    console.log(cart);
+    //Then push to localStorage  
+    localStorage.setItem("cart", JSON.stringify(cart))
+    console.log("Hi there");
+    console.log(localStorage);
+    //above working great
+    //below try if/else
+    if(selectQuantity === "0"){
+        console.log("error!")
+        // localStorage.removeItem("cartItems");
+    } else{
+        console.log('yay!')
+    };
+    //below example from mentor confusing
+    // if (selectQuantity === "0") {    
+    //     let itemIndex = cartItems.item.findIndex(item => data.id === id && data.color === selectColor)
+    //     const cartQuantity = +cartItems.item[`${itemIndex}`].quantity
+    //     const quantityToAdd = +selectQuantity
+    //       cartItems.item[`${itemIndex}`].quantity = `${cartQuantity + quantityToAdd}`
+    // }
+};
